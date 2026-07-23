f51f4e78a1e55c9a60c6e986aa6f3eaaaaa9edb4
Masande07i <magoso.nondumo@gmail.com>
2026-07-23 11:58:35 +0200

    updated

--- src/components/SavedLinks/SavedLinks.tsx
+++ src/components/SavedLinks/SavedLinks.tsx
@@ -15,32 +15,40 @@ export const SavedLinks = ({links}: SavedLinksProps) => {
   return (
     <div className= {style.card}>
        <Search/>
+     
       <Text variant='h5' style={{color:" black", fontSize:30}}>Saved Links</Text>
+      <div className= {style.content}>
       {links.length === 0 ?(
         <p className= {style.note}>Nothing saved yet</p>
       ):(
         <ul className= {style.list}>
            {links.map((link) => (
             <li key={link.id } className={style.listItem}>
-               {link.title && <h4 className={style.linkTitle}>{link.title}</h4>}
-              
+              {link.title && <h4 className={style.linkTitle}>{link.title}</h4>}
               {link.link && (
-                <a href={link.link} target="_blank" rel="noreferrer" className={style.linkUrl}>
+                <a href={link.link}  className={style.linkUrl}>
                   {link.link}
                 </a>
               )}
-              
               {link.description && <p className={style.linkDesc}>{link.description}</p>}
-              
               {link.tag && <span className={style.linkTag}>{link.tag}</span>}
+            <button type="submit">View</button>
+            <button type="submit">Update</button>
+            <button type="submit">delete</button>
             
-          
-             
             </li>
           ))}
+          
         </ul>
+       
       )
-      }
+      
+      } </div>
+        
+
+    
+
+
       
 
     </div>
